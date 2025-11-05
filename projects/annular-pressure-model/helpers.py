25c45ecc0f21404406199f9c0f852210198bd0bd
dholte <dallasholte@gmail.com>
2025-11-05 11:56:47 -0700

    Update helpers.py

--- projects/annular-pressure-model/helpers.py
+++ projects/annular-pressure-model/helpers.py
@@ -1,18 +1,19 @@
 import numpy as np
 import math
-g = 9.80665  # m/s^2
+g = 9.80665 
+
 
 def hydrostatic_profile(d, z, rho: float):
     z = np.asarray(z, dtype=float)
     return rho * g * (z[0] - z)
 
+
 def _to_seg_array(x, nseg):
     if isinstance(x, (list, tuple, np.ndarray)):
         arr = np.asarray(x, dtype=float)
-        # assume len==nseg
         return arr
-    # scalar
     return np.full(nseg, float(x))
+
 
 def _piecewise_friction(d, seg_id, grads_per_seg):
     d = np.asarray(d, dtype=float)
@@ -21,4 +22,20 @@ def _piecewise_friction(d, seg_id, grads_per_seg):
     grad_interval = grads_per_seg[seg_id[1:]]  # gradient associated with each interval
     Pf = np.concatenate(([0.0], np.cumsum(grad_interval * dd)))
     return Pf
+
+
+def total_pressure_profile(d, z, rho: float, Pf):
+    d = np.asarray(d, dtype=float)
+    z = np.asarray(z, dtype=float)
+    Pf = np.asarray(Pf, dtype=float)
+    Ps = hydrostatic_profile(d, z, rho)
+    Ptot = Ps + Pf
+    return Ps, Pf, Ptot
+
+
+def total_pressure_from_gradients(d, z, rho: float, seg_id, grads_per_seg):
+    Pf = _piecewise_friction(d, np.asarray(seg_id, dtype=int), np.asarray(grads_per_seg, dtype=float))
+    Ps = hydrostatic_profile(d, z, rho)
+    Ptot = Ps + Pf
+    return Ps, Pf, Ptot
 
